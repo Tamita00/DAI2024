b44a4b34989302037854a498a8b97dfb9053633a
TAMARA YAEL DRATLER <47807159@ort>
2024-04-04 12:05:48 -0300

    4/4

--- TP1/src/ej08.js
+++ TP1/src/ej08.js
@@ -1,119 +1,14 @@
-/* Módulo OMDBWrapper*/
-import axios from "axios";
+import {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID} from "./modules/omdb-wrapper.js";
+let resultado = null;
 
-const APIKEY = "288a0ef8";
-
-const BASE_URL = "https://www.omdbapi.com/";
-
-const OMDBSearchByPage = async (searchText, page = 1) => {
-
-let returnObject = {
-respuesta : false,
-cantidadTotal : 0,
-datos : {}
-};
-
-const OMDBSearchByPage = async (searchText, page = 1) => {
-    try {
-        const response = await axios.get(BASE_URL, {
-            params: {
-                apikey: APIKEY,
-                s: searchText,
-                page: page
-            }
-        });
-        
-        const { data } = response;
-        
-        return {
-            respuesta: true,
-            cantidadTotal: parseInt(data.totalResults),
-            datos: data.Search
-        };
-    } catch (error) {
-        console.error("Error en OMDBSearchByPage:", error.message);
-        return {
-            respuesta: false,
-            cantidadTotal: 0,
-            datos: {}
-        };
-    }
-};
-
-return returnObject;
-};
+resultado =  await OMDBSearchByPage("cars", 1);
+console.log("OMDBSearchByPage: ", resultado);
 
 
-const OMDBSearchComplete = async (searchText) => {
-let returnObject = {
-respuesta : false,
-cantidadTotal : 0,
-datos : {}
-};
-const OMDBSearchComplete = async (searchText) => {
-    try {
-        const response = await axios.get(BASE_URL, {
-            params: {
-                apikey: APIKEY,
-                s: searchText
-            }
-        });
-        
-        const { data } = response;
-        
-        return {
-            respuesta: true,
-            cantidadTotal: parseInt(data.totalResults),
-            datos: data.Search
-        };
-    } catch (error) {
-        console.error("Error en OMDBSearchComplete:", error.message);
-        return {
-            respuesta: false,
-            cantidadTotal: 0,
-            datos: {}
-        };
-    }
-};
+let resultado2 = null;
+resultado2 =  await OMDBSearchComplete("cars");
+console.log("OMDBSearchComplete: ", resultado2);
 
-return returnObject;
-};
-
-
-const OMDBGetByImdbID = async (imdbID) => {
-let returnObject = {
-respuesta : false,
-cantidadTotal : 0,
-datos : {}
-};
-const OMDBGetByImdbID = async (imdbID) => {
-    try {
-        const response = await axios.get(BASE_URL, {
-            params: {
-                apikey: APIKEY,
-                i: imdbID
-            }
-        });
-        
-        const { data } = response;
-        
-        return {
-            respuesta: true,
-            cantidadTotal: 1,
-            datos: data
-        };
-    } catch (error) {
-        console.error("Error en OMDBGetByImdbID:", error.message);
-        return {
-            respuesta: false,
-            cantidadTotal: 0,
-            datos: {}
-        };
-    }
-};
-
-return returnObject;
-};
-
-
-export {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID};
+let resultado3 = null;
+resultado3 =  await OMDBGetByImdbID("288a0ef8");
+console.log("OMDBGetByImdbID: ",  resultado3);
